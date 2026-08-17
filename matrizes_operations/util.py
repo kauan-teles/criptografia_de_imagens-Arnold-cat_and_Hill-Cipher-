d64ac810d3bb634ecc053eeb59b84694b380e60e
kauan-teles <kauanteles169@gmail.com>
2026-08-17 13:21:37 -0300

    util

--- matrizes_operations/util.py
+++ matrizes_operations/util.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import numpy as np
+
+
 def periodo(matriz_base, n):
     current = matriz_base.copy()
     I = np.eye(2, dtype=int)  # Matriz Identidade 2x2
@@ -9,12 +11,13 @@ def periodo(matriz_base, n):
     while not np.array_equal(current, I):
         current = np.dot(current, matriz_base) % n
         passos += 1
-        
+    print(passos)
     return passos
     
     
 def expoente_modular(A, k, n):
-    result = np.identity(2, dtype=int)
+    t = A.shape[0]
+    result = np.identity(t, dtype=int)
     base = A.astype(object)
     
     while k > 0:
@@ -56,17 +59,37 @@ def calcular_inversa(matriz):
     
     return matriz_inversa_modular.astype(np.uint8)
 
-def image_to_array(path):
+def image_to_array(path, quadrada=True):
+    
     try:
-        img = Image.open(path)
+        img = Image.open(path).convert('RGB')
+        if quadrada:
+            img = img.resize((min(img.size), min(img.size)))
         img_array = np.array(img)
         return img_array
     except:
         print("Impossivel carregar a imagem")
         return False
 
+def alterar_imagem_pixel_save(path):
+    import random
+    import re
+    
+    imagem_original = image_to_array(path)
+    img = imagem_original.copy()
+    n = img.shape[0]
+    x = random.randint(0, n-1)
+    y = random.randint(0, n-1)
+    r, g, b = map(int, img[x][y])
+    R = (r*r+g) % 256
+    G = (r*g + b) % 256
+    B = (b*3*r + g) % 256
+    img[x, y] = (R, G, B)
+    newpath = path.replace(".png", "_modificada_pixel.png")
+    recuperar_imagem(img).save(newpath)
+    
+    
+    
 if __name__ == '__main__':
-    array = np.array([[1, 1], [1, 2]])
-    inversa = calcular_inversa(array)
-    print(periodo(array, 750))
-    print(inversa)
+    matriz = np.array([[1, 0],[0, 3]])
+    print(periodo(matriz, 250))
